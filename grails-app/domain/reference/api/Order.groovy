df5c0e5e625ebd477a8fa3708879c99e897250d8
Glairton Santos <glairtons@gmail.com>
2017-05-23 23:06:16 -0300

    first routes api domain

---
+++ grails-app/domain/reference/api/Order.groovy
@@ -0,0 +1,27 @@
+package reference.api
+
+import grails.rest.*
+@Resource(uri='/api/orders',formats=['json','xml'])
+class Order {
+    
+    Long id
+    String stock
+    String side
+    Double price
+    Long size
+    
+    static mapping = {
+        version false
+        table 'orders'
+        id column: 'id', generator:'native', params:[sequence:'order_seq']
+      }
+
+    
+    static constraints = {
+        stock blank:false
+        side blank:false
+        price blank:false
+        size blank:false
+
+    }
+}
